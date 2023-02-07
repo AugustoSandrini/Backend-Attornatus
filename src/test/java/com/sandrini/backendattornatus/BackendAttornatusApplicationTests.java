6b0be4fcd8ce20edf2035b2726c998082268c956
Augusto Sandrini <augustosandrini2@gmail.com>
2023-02-07 13:05:49 -0300

    commit inicial do projeto

---
+++ src/test/java/com/sandrini/backendattornatus/BackendAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandrini.backendattornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
