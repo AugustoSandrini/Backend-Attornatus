2e337e99f71d12170f53cb2490e904b2dffe192f
Augusto Sandrini <augustosandrini2@gmail.com>
2023-02-17 13:03:18 -0300

    Refact: removendo annotation do repository

--- src/main/java/com/sandrini/backendattornatus/repository/PessoaRepository.java
+++ src/main/java/com/sandrini/backendattornatus/repository/PessoaRepository.java
@@ -2,8 +2,7 @@ package com.sandrini.backendattornatus.repository;
 
 import com.sandrini.backendattornatus.models.Pessoas;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface PessoaRepository extends JpaRepository<Pessoas, Long> {
 }
